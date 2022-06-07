76e99f92dd2038b3079806ec2452d9eeb6074b6a
chiUwU <97713639+PhamYenChi3@users.noreply.github.com>
2022-06-07 20:36:35 +0700

    lab5

---
+++ Run.java
@@ -0,0 +1,13 @@
+package lab5;
+public class Run {
+    public static void main(String[] args) {
+        Cylinder cylinder = new Cylinder() {
+            @Override
+            public double getArea() {
+                return super.getArea();
+            }
+        };
+        System.out.println(cylinder.getArea());
+    }
+}
+
